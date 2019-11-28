eb0e88e01403e1d880d9131508bf6fc43bdf91bb
Mark Hatch <mark_hatch@bose.com>
2019-11-27 16:53:40 -0800

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+import os.path
+
+class PackageA(ConanFile):
+    name = "PackageA"
+    version = "1.0.0"
+    settings = "os", "compiler", "build_type", "arch"
+    scm = {
+        "type": "git",
+        "url": "git@github.com:Mark-Hatch-Bose/ConanPackageA.git",
+        "revision": "auto"
+    }
+    exports = "file.txt"
+        
+    def package(self):
+        self.copy("*")
+
+    def package_id(self):
+        self.info.requires.full_package_mode()
